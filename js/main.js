05c6b9c0dd28ff5f1ee2cedd99b4acbdb20e7739
abhibhatt48 <47849440+abhibhatt48@users.noreply.github.com>
2020-06-21 14:23:53 +0530

    yes

--- js/main.js
+++ js/main.js
@@ -7,9 +7,9 @@
 
 var ml4 = {};
 ml4.opacityIn = [0,1];
-ml4.scaleIn = [0.2, 1];
-ml4.scaleOut = 3;
-ml4.durationIn = 1200;
+ml4.scaleIn = [0.2,0];
+ml4.scaleOut = 0;
+ml4.durationIn = 3000;
 ml4.durationOut = 600;
 ml4.delay = 500;
 
