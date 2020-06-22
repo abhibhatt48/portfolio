c1a0c8bc49eaaa8747036597c18a54c7ff092506
abhibhatt48 <47849440+abhibhatt48@users.noreply.github.com>
2020-06-22 14:40:05 +0530

    ab

--- js/main.js
+++ js/main.js
@@ -8,11 +8,11 @@ anime.timeline({loop: true})
     opacity: [0,1],
     easing: "easeInOutQuad",
     duration: 2250,
-    delay: (el, i) => 50 * (i+1)
+    delay: (el, i) => 25 * (i+1)
   }).add({
     targets: '.ml3',
     opacity: 0,
     duration: 1000,
     easing: "easeOutExpo",
-    delay: 1000
+    delay: 500
   });
